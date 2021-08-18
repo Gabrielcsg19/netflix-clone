0ffd3fd6d9c1008148f01a5df91fd514e6b3d39c
Gabriel Souza <gabrielcsg19@hotmail.com>
2021-08-18 16:08:51 -0300

    Variáveis de ambiente criadas

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,20 +1,5 @@
-const API_KEY = 'b56f43d369bba7c8926144ac26f8a29a';
-const API_BASE = 'https://api.themoviedb.org/3';
-
-/*
-- Originais da Netflix
-- Recomendados (tending)
-- Em alta (top rated)
-- Ação
-- Comédia
-- Terror
-- Romance
-- Documentário
-*/
-
-// eslint-disable-next-line
 const basicFetch = async (endpoint) => {
-    const request = await fetch(`${API_BASE}${endpoint}`);
+    const request = await fetch(`${process.env.REACT_APP_TMDB_ENDPOINT}${endpoint}`);
     const json = await request.json();
     return json;
 }
@@ -26,42 +11,42 @@ export default {
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para você',
-                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
             },
         ]
     },
@@ -70,10 +55,10 @@ export default {
         if (movieId) {
             switch(type) {
                 case 'movie':
-                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
                     break;
                 case 'tv':
-                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
                     break;
                 default:
                     info = null;
